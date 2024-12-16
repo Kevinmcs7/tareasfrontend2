055661f119303eaa85d22e5780e317889594e647
Kevin <kevinmgue@gmail.com>
2024-12-16 12:35:00 -0600

    cambio en axios credenciales

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const instance = axios.create ({
     baseURL: 'https://tareasbackend2-production.up.railway.app/api',
-    withCredentials: true 
 })
 
 export default instance
